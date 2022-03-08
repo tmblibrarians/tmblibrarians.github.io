6c261aad7baadcb2ce3eeda4a08963f77c267b65
imagallanes24 <89544214+imagallanes24@users.noreply.github.com>
2022-03-08 13:20:57 -0800

    php test 2/8

---
+++ registration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Registration | PHP</title>
+</head>
+<body>
+
+<div>
+	<form action="registration.php" method="post">
+		<div class="container">
+			<h1>Registration</h1>
+			<p>Fill up the form with correct values.</p>
+			<label for="firstname"><b>First Name</b></label>
+			<input type="text" name="firstname" required>
+			
+			<label for="lastname"><b>Last Name</b></label>
+			<input type="text" name="lastname" required>
+			
+			<label for="email"><b>Email Address</b></label>
+			<input type="email" name="email" required>
+			
+			<label for="phonenumber"><b>Phone Number</b></label>
+			<input type="text" name="phonenumber" required>
+			
+			<label for="password"><b>Password</b></label>
+			<input type="password" name="password" required>
+		</div>
+	</form>
+</div>
+
+</body>
+</html>
